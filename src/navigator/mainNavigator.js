cf9a08da861fb3fc1118a4d7921b98832c72fe08
bgoldste <bengoldstein9@gmail.com>
2021-03-17 17:24:44 -0700

    Update screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from "@react-navigation/native"
 import SplashScreen from "../features/SplashScreen"
 import SideMenu from "./sideMenu"
 //@BlueprintImportInsertion
+import SplashScreen1215646Navigator from '../features/SplashScreen1215646/navigator';
 
 /**
  * new navigators can be imported here
@@ -14,12 +15,21 @@ const AppNavigator = {
   //@BlueprintNavigationInsertion
 
   /** new navigators can be added here */
-  SplashScreen: {
-    screen: SplashScreen
+  SplashScreen1215646: {
+    screen: SplashScreen1215646Navigator
   }
 }
 
-const Drawer = createDrawerNavigator()
+
+const Drawer = createDrawerNavigator(
+    {
+        ...AppNavigator,
+    },
+    {
+        contentComponent: SideMenu,
+        initialRouteName: "SplashScreen1215646", // Splash Screen
+    },
+);
 
 const AppContainer = () => {
   return (
